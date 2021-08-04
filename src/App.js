78f4330e13ac321512f045fbe94fd6bf68443b6b
lequan1301 <quanchinh13012000@gmail.com>
2021-08-04 10:49:20 +0700

    Update title 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Lê Quân
+         Lê Quân love Diễm Quỳnh
         </p>
         <a
           className="App-link"
